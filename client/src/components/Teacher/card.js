8da114b9c8708a471f5a30ac01c6f8262bc6aca9
Andrea Garcia <andreagl995@hotmail.com>
2019-03-14 21:31:08 -0600

    agregue boton admin

---
+++ client/src/components/Teacher/card.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Card() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <h1 className="navbar-brand"> Admin.  </h1>
+      <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+
+
+        <li className="nav-item active">
+          <Link className="nav-link" to="/newstudent">
+            Add Student 
+          </Link>
+        </li>
+
+
+
+      </ul>
+    </nav>
+  );
+}
+
+export default Card;
