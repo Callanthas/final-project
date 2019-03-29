9c261af130f1de016fb8996eba2654040fbda6f1
Andrea Garcia <andreagl995@hotmail.com>
2019-03-28 22:02:25 -0600

    design1

--- client/src/components/LoginNav/index.js
+++ client/src/components/LoginNav/index.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import  ADMIN  from "../../assets/icons/ADMIN.png";
+import  STUDENT  from "../../assets/icons/STUDENT.png";
+import  LEAD  from "../../assets/icons/LEAD.png";
+import { LOGO } from "../../assets/icons/LOGO-01.png";
+
+
+
 /* import Dropdown from 'react-bootstrap/Dropdown';
 import FormControl from 'react-bootstrap/FormControl'; */
 
@@ -18,16 +25,53 @@ class LoginNav extends Component {
 
   render() {
     return (
-      <nav
-        className="navbar navbar-expand-lg navbar-dark bg-dark"
-        id="login-nav"
-      >
-        <Link to="/login/student">Student</Link>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <Link to="/login/supervisor">Supervisor</Link>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <Link to="/login/admin">Administrator</Link>
-      </nav>
+
+      <div class="container">
+        <div class="card-deck mb-3 text-center">
+          <div class="card mb-4 shadow-sm">
+            <div class="card-header">
+
+              <h4 class="my-0 font-weight-normal">Administrator</h4>
+            </div>
+            <div class="card-body">
+              <img className="rounded-circle" src={ADMIN} alt="admin" width="200" height="140">
+              </img>
+
+              <p className="themed-grid-col" >Click to login as Administrator. An Administrator can add new students, leads and projects.</p>
+
+              <Link className="themed-grid-col" to="/login/admin">Administrator</Link>
+            </div>
+          </div>
+
+          <div class="card mb-4 shadow-sm">
+            <div class="card-header">
+              <h4 class="my-0 font-weight-normal">Student</h4>
+            </div>
+            <div class="card-body">
+              <img className="rounded-circle" src={STUDENT} alt="student" width="180" height="140">
+              </img>
+              <p className="themed-grid-col" >As a student you can see your project, leader, check-in and check-out from your community service project.</p>
+              <Link className="themed-grid-col" to="/login/student">Student</Link>
+            </div>
+          </div>
+
+
+          <div class="card mb-4 shadow-sm">
+            <div class="card-header">
+              <h4 class="my-0 font-weight-normal">Leader</h4>
+            </div>
+            <div class="card-body">
+              <img className="rounded-circle" src={LEAD} alt="lead" width="160" height="140" />
+
+              <p className="themed-grid-col" >Check in as a leader and/or teacher.</p>
+              <Link className="themed-grid-col" to="/login/supervisor">Leader</Link>
+            </div>
+
+
+          </div>
+        </div>
+
+      </div>
     );
   }
 }
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import API from "../utils/API";
 import LoginNav from "../components/LoginNav";
+import logo from "../assets/icons/LOGO-01.png";
+
 
 class Login extends Component {
   constructor(props) {
@@ -12,19 +14,24 @@ class Login extends Component {
   render() {
     return (
       <div>
-        <LoginNav />
         <div className="card">
           <div className="card-body">
-            Hello! <br /> <br />
-            Welcome to the USSR - Unified Social Service Register. Please log-in
-            by choosing your role from the links in the navigation bar above.{" "}
+
+            <img className="rounded-circle" src={logo} alt="logo" width="250" height="200">
+            </img>
+            <h1>Hello! </h1>
+            <div>Welcome to the UCSR - Unified Community Service Register. Please log-in
+                        by choosing your role from the links in the navigation bar below.
+  </div>
             <br />
-            <br />
-            The USSR system aims to simplify the tracking of Social Service
-            hours for universities, students and institutions.
-            <br />
+            <div>
+              The UCSR system aims to simplify the tracking of Social Service
+                          hours for universities, students and institutions.
+</div>
           </div>
         </div>
+        <br /><br />
+        <LoginNav />
       </div>
     );
   }
